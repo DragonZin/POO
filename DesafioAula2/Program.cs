94c197f80e9a9f21ee3f19245a1e8e31ac68a706
Enzo <enzomtrevisan@gmail.com>
2025-08-23 18:15:32 -0300

    Desafio Aula 2

---
+++ DesafioAula2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        string nomeProduto = "Notebook Gamer";
+        int quantidade = 15;
+        double precoUnitario = 4999.90;
+        bool ativoParaVenda = true;
+
+        Console.WriteLine("Produto: " + nomeProduto);
+        Console.WriteLine("Quantidade em estoque: " + quantidade);
+        Console.WriteLine("Preço unitário: R$ " + precoUnitario);
+        Console.WriteLine("Ativo para venda: " + ativoParaVenda);
+    }
+}
