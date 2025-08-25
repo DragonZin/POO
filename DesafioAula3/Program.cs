e374113002e1ee6c183c63e65f982b8ccfbc7a3e
Enzo <enzomtrevisan@gmail.com>
2025-08-25 09:41:07 -0300

    Desafio Aula 3

---
+++ DesafioAula3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite a senha: ");
+        string senha = Console.ReadLine();
+
+        if (senha == "1234")
+        {
+            Console.WriteLine("Acesso liberado.");
+        }
+        else
+        {
+            Console.WriteLine("Senha incorreta.");
+        }
+    }
+}
