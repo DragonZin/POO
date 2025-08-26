7d51a9b5cadbe1d962952ba1febf5102ccf355aa
Enzo <enzomtrevisan@gmail.com>
2025-08-25 23:03:25 -0300

    Desafio aula 4

---
+++ DesafioAula4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double soma = 0;
+
+        for (int i = 1; i <= 4; i++)
+        {
+            Console.Write($"Digite a nota da prova {i}: ");
+            double nota = double.Parse(Console.ReadLine());
+            soma += nota;
+        }
+
+        double media = soma / 4;
+
+        Console.WriteLine($"\nMédia: {media:F2}");
+
+        if (media >= 6)
+            Console.WriteLine("Aluno aprovado!");
+        else
+            Console.WriteLine("Aluno reprovado!");
+    }
+}
