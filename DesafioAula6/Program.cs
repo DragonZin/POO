0f9652014b539b0c25cfa6ba234709f3da9aa293
Enzo <enzomtrevisan@gmail.com>
2025-09-09 16:27:37 -0300

    Desafio aula 6

---
+++ DesafioAula6/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Temperatura t1 = new Temperatura(0, Escala.Celsius);
+        Console.WriteLine($"0°C em Fahrenheit = {t1.EmFahrenheit():F2}°F");
+
+        Temperatura t2 = new Temperatura(32, Escala.Fahrenheit);
+        Console.WriteLine($"32°F em Celsius = {t2.EmCelsius():F2}°C");
+    }
+}
---
+++ DesafioAula6/Temperatura.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public enum Escala
+{
+    Celsius,
+    Fahrenheit
+}
+
+public class Temperatura
+{
+    private double valor;
+    private Escala escala;
+
+    public Temperatura(double valor, Escala escala)
+    {
+        this.valor = valor;
+        this.escala = escala;
+    }
+
+    public double EmCelsius()
+    {
+        if (escala == Escala.Celsius)
+            return valor;
+        else
+            return (valor - 32) * 5.0 / 9.0;
+    }
+
+    public double EmFahrenheit()
+    {
+        if (escala == Escala.Fahrenheit)
+            return valor;
+        else
+            return (valor * 9.0 / 5.0) + 32;
+    }
+
+    public override string ToString()
+    {
+        return $"{valor:F2} {escala}";
+    }
+}
